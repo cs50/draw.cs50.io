e1ccba03f673865fc006913346d962cf698de80c
Dan Coffey <danielpcoffey@gmail.com>
2018-10-25 22:46:55 -0400

    tweaks

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ var pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    port: process.env.MYSQL_PORT,
-    database: process.env.MYSQL_DB,
-    supportBigNumbers: true
+    database: process.env.MYSQL_DATABASE,
 });
 
 
